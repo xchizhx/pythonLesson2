69788c0106781585e086ed6b6e91be34b3d00dc9
Ivani_Chizhs <ivanchizhina@gmail.com>
2023-08-07 11:49:22 +0300

    Dz 2.2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import time
 
-h = int(input())
+j = int(input())
 
 while True:
-    time.sleep(1)
-    h -= 1
-    print(h)
-    if h == 0:
-        print("Бум!")
-        break
+    for i in range(j):
+        if i % 2 == 0:
+            print("Первый")
+        else:
+            print("Второй")
+    print("Расчёт окончен")
+    break
